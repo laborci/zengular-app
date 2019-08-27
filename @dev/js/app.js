668c0240e311c207d6b05db54e91ebea3e10bb96
Gergely Laborci <gergely@laborci.hu>
2019-08-27 14:53:33 +0200

    no message

--- @dev/js/app.js
+++ @dev/js/app.js
@@ -1,5 +1,4 @@
 import Application from "zengular/core/application";
-import User        from "models/user";
 import "bricks/todo/brick";
 import "bricks/task/brick";
 import "bricks/task-list/brick";
@@ -7,9 +6,3 @@ import "bricks/task-list/brick";
 new (class extends Application {
 	run() {}
 })();
-
-User.repository.store({name: "elvis", email: "elvis@elvis.hu"});
-User.repository.store({name: "ewer", email: "elvis@elvis.hu"});
-console.log(User.repository.storage)
-
-console.log(User.repository.get(4)._.name)
